28487020281866599e724bce7abd47e084f180eb
LariFC <105297596+LariFC@users.noreply.github.com>
2023-12-03 11:46:30 -0300

    aaaa

---
+++ RelacionamentoHeranca/Controllers/NaoPerecivelController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RelacionamentoHeranca.Data;
+using RelacionamentoHeranca.Migrations;
+using RelacionamentoHeranca.Models;
+using System.Linq;
+
+public class NaoPerecivelController : Controller
+{
+    private readonly EstoqueContext _context;
+
+    public NaoPerecivelController(EstoqueContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.NaoPereciveis.OrderBy(i => i.Nome).ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Create([Bind("Nome", "Valor", "Quantidade", "Cor", "Tamanho", "Material")] NaoPerecivel naoperecivel)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(naoperecivel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+        }
+        catch (DbUpdateException)
+        {
+            ModelState.AddModelError("", "Não foi possível cadastrar ");
+        }
+        return View(naoperecivel);
+    }
+
+    public async Task<ActionResult> Edit(long id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var naoperecivel = await _context.NaoPereciveis.SingleOrDefaultAsync(i => i.ProdutoID == id);
+        if (naoperecivel == null)
+        {
+            return NotFound();
+        }
+        return View(naoperecivel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(long? id, [Bind("ProdutoID", "Nome", "Valor", "Quantidade", "Cor", "Tamanho", "Material")] NaoPerecivel naoperecivel)
+    {
+        if (id != naoperecivel.ProdutoID)
+        {
+            return NotFound();
+        }
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(naoperecivel);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (!NaoPerecivelExists(naoperecivel.ProdutoID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        return View(naoperecivel);
+    }
+
+    private bool NaoPerecivelExists(long? produtoID)
+    {
+        var naoperecivel = _context.NaoPereciveis.FirstOrDefault(i => i.ProdutoID == produtoID);
+        if (naoperecivel == null)
+            return false;
+        return true;
+    }
+    public async Task<ActionResult> Details(long id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var naoperecivel = await _context.NaoPereciveis.SingleOrDefaultAsync(i => i.ProdutoID == id);
+        if (naoperecivel == null)
+        {
+            return NotFound();
+        }
+        return View(naoperecivel);
+    }
+    public async Task<IActionResult> Delete(long? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var naoperecivel = await _context.NaoPereciveis.SingleOrDefaultAsync(i => i.ProdutoID == id);
+        if (naoperecivel == null)
+        {
+            return NotFound();
+        }
+        return View(naoperecivel);
+    }
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(long? id)
+    {
+        var naoperecivel = await _context.NaoPereciveis.SingleOrDefaultAsync(i => i.ProdutoID == id);
+        _context.Produtos.Remove(naoperecivel);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+
+}
---
+++ RelacionamentoHeranca/Migrations/20231201161003_initial.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelacionamentoHeranca.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Valor",
+                table: "Produtos",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cor",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataValidade",
+                table: "Produtos",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Material",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Peso",
+                table: "Produtos",
+                type: "real",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sabor",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tamanho",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cor",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "DataValidade",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Material",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Peso",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Sabor",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Tamanho",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Produtos",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
